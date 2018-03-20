62b7dfe14a36c8be6b662dc618bb9bb1c823e9ed
Philipp Rößner <philipprossner@MacBook-Pro-2.local>
2018-03-20 16:14:57 +0100

    serviceworker implementiert

--- serviceworker.js
+++ serviceworker.js
@@ -1,31 +1,38 @@
-self.importScripts('team1.js');
-self.importScripts('team2.js');
+var CACHE_NAME = 'pwaMatchDay-v1';
+var urlsToCache = [
+  '/index.html',
+  '/script.js',
+  '/style.css',
+  '/assets/css/bundesliga_header.css',
+  '/assets/css/bundesliga.css',
+  '/assets/icon.svg',
+  '/img/bundesliga_icon_48.png',
+  '/img/bundesliga_icon_72.png',
+  '/img/bundesliga_icon_96.png',
+  '/img/bundesliga_icon_144.png',
+  '/img/bundesliga_icon_358x358.png'
+];
 
-self.addEventListener('install', function(event) {
-    console.log("addEventListener install");
-    event.waitUntil(
-        caches.open('v1').then(function(cache) {
-            return cache.addAll([
-                'style.css'
-            ]);
-        })
-    );
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 
-x
-  /*
-this.addEventListener('fetch', function(event) {
-    console.log("addEventListener fetch");
-    event.respondWith(
-      caches.match(event.request).catch(function() {
-        return fetch(event.request).then(function(response) {
-          return caches.open('v1').then(function(cache) {
-            cache.put(event.request, response.clone());
-            return response;
-          });  
-        });
-      })
-    );
-  });
-*/
+async function handleFetch (request) {
+  const cache = await self.caches.open(CACHE_NAME);
+  // if (request.url.endsWith("/cat.jpg")) {
+  //   return cache.match("dog.jpg");
+  // }
+  return cache.match(request);
+}
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(handleFetch(evt.request));
+});
